e74b29d0dec11e794180e627e59cc6c6444a18d7
Gaurav <dusejagaurav20@gmail.com>
2025-11-09 13:52:47 +0530

    routing topic

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
+// app.use("/", (req, res) => {
+//   res.send("Akshay world");
+// });
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Akshay", lastName: "Kumar" });
+});
+app.post("/user", (req, res) => {
+  res.send("Data has been posted successfully");
+});
+app.delete("/user", (req, res) => {
+  res.send("Data has been deleted successfully");
+});
 app.use("/test", (req, res) => {
   res.send("Hello world");
+});
+app.use("/hello", (req, res) => {
+  res.send("Hello Hello world");
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
